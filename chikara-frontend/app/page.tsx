60821c8231baccc0878d502231f7526046e759ff
Pauline-Wanjiru <gokowanjiru@gmail.com>
2023-09-24 19:08:39 +0300

    renamed the files

--- chikara-frontend/app/page.tsx
+++ chikara-frontend/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
-import Emission from './Emissions/page'
+import CarbonAvoided from './carbonChart/page'
+import CarbonEmitted from './limitChart/page'
 
 
 export default function Home() {
   return (
     <div>
-    <Emission />
+    <CarbonEmitted />
+    <CarbonAvoided />
+   
    
     </div>
    
